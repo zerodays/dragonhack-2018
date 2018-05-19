05a0a6f3301335254b2ebb989b11706a5aa3f666
GoesUp <gregor@gabrovsek.si>
2018-05-19 16:50:02 +0200

    Improved vendor recognition even more

--- server/recognition.py
+++ server/recognition.py
@@ -1,4 +1,5 @@
 from testing_vars import temp
+import difflib
 
 def get_price_from_text(dictionary):
     """
@@ -28,11 +29,13 @@ def get_vendor_name_from_text(dictionary):
     data = dictionary['responses'][0]['textAnnotations']
     receipt_text = data[0]['description'].split("\n")
     vendors = ["spar", "deichmann", "mercator", "lidl", "tus", "hofer", "interspar"]
-    for i in vendors:
-        header = (o.lower() for o in receipt_text[:10])
-        if i in header:
-            return i
-    return receipt_text[0].lower()
+    possible_vendors = []
+    for i in receipt_text[:10]:
+        i = i.lower()
+        possibilities = difflib.get_close_matches(i, vendors, cutoff=0.8)
+        if possibilities:
+            possible_vendors += possibilities
+    return possible_vendors
 
 if __name__ == '__main__':
     get_price_from_text(temp)
