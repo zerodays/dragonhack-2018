6154e66deeec41ff25a856352b58ea965904f242
GoesUp <gregor@gabrovsek.si>
2018-05-19 17:34:40 +0200

    Improved vendor recognition and added comments

--- server/recognition.py
+++ server/recognition.py
@@ -1,6 +1,7 @@
 from testing_vars import temp
 import difflib
 
+
 def get_price_from_text(dictionary):
     """
     Recognize price from text
@@ -34,7 +35,6 @@ def get_price_from_text(dictionary):
         if len(splited[0]) > 4 or len(splited[1]) != 2:
             continue
 
-
         try:
             whole = int(splited[0])
             part = int(splited[1])
@@ -49,24 +49,39 @@ def get_price_from_text(dictionary):
 
 
 def get_vendor_name_from_text(dictionary):
-    def is_close(orig_string, possible_strings, diff):
-        return difflib.get_close_matches(orig_string, possible_strings, cutoff=diff)
+    """
+    Recognize receipt vendor from text
+    :param dictionary: dictionary returned from google api
+    :return: vendor as string (lowercase)
+    """
+
+    def is_close(data, possible_strings, diff):
+        """
+        Uses difflib to check if any of the stringsis close enough to any of the predetermined vendors
+        """
+        temp_possible_vendors = []
+        for i in data:  # Checks the data if it contains something close enough to any of the vendor names
+            i = i.lower()
+            possibilities = difflib.get_close_matches(i, possible_strings, cutoff=diff)
+            if possibilities:
+                temp_possible_vendors += possibilities
+        return temp_possible_vendors
+
     data = dictionary['responses'][0]['textAnnotations']
     receipt_text = data[0]['description'].split("\n")
-    vendors = ["spar", "deichmann", "mercator", "lidl", "tuš", "hofer", "interspar", ""]
-    possible_vendors = []
-    for i in receipt_text[:20]:
-        i = i.lower()
-        possibilities = is_close(i.lower(), vendors, 0.8)
-        if possibilities:
-            possible_vendors += possibilities
-    if len(possible_vendors) == 1:
+
+    vendors = ["spar", "deichmann", "mercator", "lidl", "tuš", "hofer", "interspar", "eurospin"]
+    possible_vendors = is_close(receipt_text[:20], vendors, 0.8)
+
+    possible_vendors = list(possible_vendors)
+    if len(possible_vendors) == 1:  # If only one vendor is found, return it
         return possible_vendors[0]
-    elif len(possible_vendors) > 0:
-        possible_vendors = is_close(i.lower(), vendors, 1)
+    elif len(possible_vendors) > 0:  # If there is more than one vendor found
+        possible_vendors = is_close(receipt_text[:20], vendors, 1)
         return possible_vendors[0]
     else:
         return receipt_text[0]
+
 
 if __name__ == '__main__':
     get_price_from_text(temp)
