3de9c66d343dd239f509823bf38fe8f6d2e877bc
Vid Drobnic <vid.drobnic@gmail.com>
2018-05-19 15:37:15 +0200

    basic server

--- server/main.py
+++ server/main.py
@@ -1,23 +1,35 @@
 import image_helpers
 import requests
+from flask import Flask, request
 
-image = image_helpers.image_to_base64('../sample_images/IMG_20180519_123741.jpg')
+app = Flask(__name__)
 
-url = 'https://vision.googleapis.com/v1/images:annotate?key=AIzaSyCFA9NO1gfGYOaZuGGzFiCtFLH7fTBj-PE'
-data = {
-   'requests': [
-       {
-           'image': {
-               'content': image,
-           },
-           'features': [
-               {
-                   'type': 'TEXT_DETECTION',
-               },
-           ]
-       }
-   ]
-}
 
-r = requests.post(url, json=data)
-print(r.json())
+@app.route('/recognize', methods=['POST'])
+def recognize():
+    # print(request.data)
+    image = image_helpers.image_to_base64('../sample_images/IMG_20180519_123741.jpg')
+
+    url = 'https://vision.googleapis.com/v1/images:annotate?key=AIzaSyCFA9NO1gfGYOaZuGGzFiCtFLH7fTBj-PE'
+    data = {
+        'requests': [
+            {
+                'image': {
+                    'content': image,
+                },
+                'features': [
+                    {
+                        'type': 'TEXT_DETECTION',
+                    },
+                ]
+            }
+        ]
+    }
+
+    r = requests.post(url, json=data)
+    print(r.json())
+    return request.data
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
