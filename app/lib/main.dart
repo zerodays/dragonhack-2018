1f6e07fd7b7dae8abe654d51e4dd7328528deab8
Matej Marinko <matejmarinko123@gmail.com>
2018-05-19 12:14:15 +0200

    fix camera crash

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,8 +1,17 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
 import 'camera_view.dart';
 
-void main() => runApp(new MyApp());
+Future<Null> main() async {
+  try {
+    cameras = await availableCameras();
+  } on CameraException catch (e) {
+    logError(e.code, e.description);
+  }
 
+  runApp(new MyApp());
+}
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
